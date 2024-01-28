345f2081661460bfba00432e2766878c815dea59
PuppyCute-YTB <89437880+PuppyCute-YTB@users.noreply.github.com>
2024-01-28 20:09:17 +0700

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -4,6 +4,7 @@ chmod +x stockfish13
 chmod +x stockfish13m
 chmod +x fairy-stockfish-largeboard_x86-64-modern
 chmod +x fairy-stockfish-largeboard_x86-64
+set TOKEN=lip_gPYb7vtYY7H8zhq8pddd
 node mbooks.js &
 node mbooks2.js &
 node server.js
